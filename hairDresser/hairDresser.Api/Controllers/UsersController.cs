f7603a2dc4455fa8d9ffda036fa070323d0a9585
david <daviddserb@yahoo.com>
2022-09-25 06:39:33 +0300

    BeforeAddingOAuth

---
+++ hairDresser/hairDresser.Api/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hairDresser.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+
+    }
+}
