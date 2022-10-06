c5abd8a3cda4a817c7da886df3f505175d84a1dc
david <daviddserb@yahoo.com>
2022-10-06 08:39:34 +0300

    beforeChangeSelectionDateForAppointment

--- hairDresser/hairDresser.Application/Employees/Queries/GetEmployeeFreeIntervalsForAppointmentByDate/GetEmployeeFreeIntervalsForAppointmentByDateQuery.cs
+++ hairDresser/hairDresser.Application/Employees/Queries/GetEmployeeFreeIntervalsForAppointmentByDate/GetEmployeeFreeIntervalsForAppointmentByDateQuery.cs
@@ -12,6 +12,8 @@ namespace hairDresser.Application.Employees.Queries.GetEmployeeFreeIntervalsForA
     {
         public int EmployeeId { get; set; }
 
+        public int Year { get; set; }
+        public int Month { get; set; }
         // Date = nr. zilei din luna (1, 15, ..., 29, ..., 31).
         public int Date { get; set; }
 
