59d0aaca3e794d15a06bc0a4523b19498b58c7b5
david <daviddserb@yahoo.com>
2023-09-06 20:09:33 +0300

    RemoveEmployeeFromWorkingIntervalGetDto

--- hairDresser/hairDresser.Api/Dto/WorkingDayDtos/WorkingDayGetDto.cs
+++ hairDresser/hairDresser.Api/Dto/WorkingDayDtos/WorkingDayGetDto.cs
@@ -2,7 +2,7 @@
 {
     public class WorkingDayGetDto
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string Name { get; set; }
     }
 }
--- hairDresser/hairDresser.Api/Dto/WorkingIntervalDtos/WorkingIntervalGetDto.cs
+++ hairDresser/hairDresser.Api/Dto/WorkingIntervalDtos/WorkingIntervalGetDto.cs
@@ -1,17 +1,11 @@
-﻿
-using hairDresser.Presentation.Dto.EmployeeDtos;
-using hairDresser.Presentation.Dto.WorkingDayDtos;
+﻿using hairDresser.Presentation.Dto.WorkingDayDtos;
 
 namespace hairDresser.Presentation.Dto.WorkingIntervalDtos
 {
     public class WorkingIntervalGetDto
     {
         public int Id { get; set; }
-
         public WorkingDayGetDto WorkingDay { get; set; }
-
-        public EmployeeGetDto Employee { get; set; }
-
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
     }
